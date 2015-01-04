cfdddc9bc08a1abeced976e25cc47d44132817f0
jrofurtado <jrofurtado@gmail.com>
2015-01-04 16:59:22 -0100

    initial version

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
 dart-appcache
 =============
 
-A dart transformer to generate an appcache file
+_A dart transformer to generate an appcache file_
+
+This transformer generates an appcache file with all the project resources and modifies the entry points to reference it.
+
+# Configuring
+    
+Add the transformer to your pubspec.yaml:
+
+    transformers:
+    - appcache
+        entry_points: web/index.html
+    
+(Assuming you already added this package to your pubspec.yaml file.)
+
+# Options
+
+## Specify a list of files that should be ignored
+
+    transformers:
+    - appcache:
+        entry_points: web/index.html
+        exclude:
+          - web/file1
+          - web/dir1
+
+## Specify a list of urls that should also be cached
+
+    transformers:
+    - appcache:
+        entry_points: web/index.html
+        cache:
+          - web/packages/web_components/webcomponents.min.js
+          - web/packages/web_components/dart_support.js
+          - web/packages/polymer/src/js/polymer/polymer.min.js
+          - https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js
+
+## Specify the network part (default is '*')
+
+    transformers:
+    - appcache:
+        entry_points: web/index.html
+        network:
+          - web/file1
+          - web/file2
+
+## Specify the fallback part (no default)
+
+    transformers:
+    - appcache:
+        entry_points: web/index.html
+        fallback:
+          - / web/offine.html
---
+++ lib/appcache.dart
@@ -0,0 +1,113 @@
+library appcache;
+
+import 'package:barback/barback.dart';
+import 'dart:async' show Future;
+import 'package:html5lib/parser.dart';
+
+class TransformOptions {
+  List<String> entryPoints;
+  List<String> exclude;
+  List<String> cache;
+  List<String> network;
+  List<String> fallback;
+  bool debug;
+}
+
+class AppCacheTransformer extends AggregateTransformer {
+  TransformOptions options;
+  AppCacheTransformer(this.options);
+  AppCacheTransformer.asPlugin(BarbackSettings settings) : this(_parseSettings(settings));
+
+  static List<String> _settings2List(value, name) {
+    var list = [];
+    if (value != null) {
+      bool error;
+      if (value is List) {
+        list = value;
+        error = value.any((e) => e is! String);
+      } else if (value is String) {
+        list = [value];
+        error = false;
+      } else {
+        error = true;
+      }
+      if (error) print('Invalid value for "$name" in the appcache transformer.');
+    }
+    return list;
+  }
+
+  static TransformOptions _parseSettings(BarbackSettings settings) {
+    var args = settings.configuration;
+    TransformOptions options = new TransformOptions();
+    options.entryPoints = _settings2List(args["entry_points"], "entry_points");
+    options.exclude = _settings2List(args["exclude"], "exclude");
+    options.cache = _settings2List(args["cache"], "cache");
+    options.network = _settings2List(args["network"], "network");
+    options.fallback = _settings2List(args["fallback"], "fallback");
+    options.debug = settings.mode.name == "debug";
+    print("DEBUG*******************${options.debug}");
+    return options;
+  }
+
+  Future apply(AggregateTransform transform) {
+    return transform.primaryInputs.toList().then((assets) {
+      if (!options.debug && transform.key == "entry-points") {
+        for (var i in assets) {
+          return i.readAsString().then((content) {
+            var document = parse(content);
+            var html = document.getElementsByTagName("html").first;
+            html.attributes["manifest"] = "app.appcache";
+            transform.addOutput(new Asset.fromString(i.id, document.outerHtml));
+          });
+        }
+      } else if (!options.debug) {
+        var buffer = new StringBuffer()
+            ..writeln('CACHE MANIFEST')
+            ..writeln('#Generated: ${new DateTime.now()}')
+            ..writeln('CACHE:');
+        for (var i in options.cache) {
+          if (i.startsWith("web/")) buffer.writeln(i.substring(4)); else buffer.writeln(i);
+        }
+        for (var i in assets) {
+          buffer.writeln(i.id.path.substring(4));
+        }
+        buffer.writeln('NETWORK:');
+        if (options.network.isEmpty) {
+          buffer.writeln('*');
+        } else {
+          for (var i in options.network) {
+            if (i.startsWith("web/")) buffer.writeln(i.substring(4)); else buffer.writeln(i);
+          }
+        }
+        if (options.fallback.isNotEmpty) {
+          buffer.writeln('FALLBACK:');
+          for (var i in options.fallback) {
+            List<String> line = i.split(" ");
+            String converted = "";
+            String separator = "";
+            for (var j in line) {
+              if (j.startsWith("web/")) converted += separator + j.substring(4); else converted += separator + j;
+              separator = " ";
+            }
+            buffer.writeln(converted);
+          }
+        }
+        var id = new AssetId(transform.package, "web/app.appcache");
+        transform.addOutput(new Asset.fromString(id, buffer.toString()));
+      }
+    });
+  }
+
+  String classifyPrimary(AssetId id) {
+    if (options.debug) return null;
+    for (var i in options.entryPoints) {
+      if (id.path == i) return "entry-points";
+    }
+    if (id.path.endsWith("appcache")) return null;
+    if (!id.path.startsWith("web/")) return null;
+    for (var i in options.exclude) {
+      if (id.path.startsWith(i)) return null;
+    }
+    return "cache";
+  }
+}
